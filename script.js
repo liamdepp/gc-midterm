cc5b577c9c71c9c948c392aeb708d2ba65bd78fc
Liam Deppong <liamdepp0605@gmail.com>
2019-11-01 11:35:50 -0400

    Pre-lunch push

--- script.js
+++ script.js
@@ -71,6 +71,9 @@ function getTotal() {
   document.getElementById('subTotal').innerText = `Subtotal: $${subTotal.toFixed(2)}`;
   document.getElementById('tax').innerText = `Tax: $${tax.toFixed(2)}`;
   document.getElementById('total').innerText = `Total: $${total.toFixed(2)}`;
+  document.querySelector(".amountDue").innerText = `Total: $${total.toFixed(
+    2
+  )}`;
 }
 
 //food item buttons/cards container class
@@ -103,13 +106,82 @@ foodItems.forEach((foodItem, index) => {
 
 function display() {
   document.querySelector("#cart").innerText = "";
-  // cart.forEach((item, index) => {
-  for (const item of cart) {
+  cart.forEach((item, index) => {
     const newItem = document.createElement("div");
+    const deleteIcon = document.createElement("span");
+
+    deleteIcon.innerHTML = `<i class="fas fa-times"></i>`;
     newItem.classList.add("cartItem");
+    newItem.setAttribute("index", index);
+    deleteIcon.setAttribute("index", index);
     newItem.innerText = `${item.name}: $${item.price.toFixed(2)}`;
 
+    newItem.appendChild(deleteIcon);
     document.querySelector("#cart").appendChild(newItem);
 
+    console.log(cart);
+  });
+
+    
+  
+  /*for (const item of cart) {
+    const newItem = document.createElement("div");
+    const deleteIcon = document.createElement("span");
+    deleteIcon.innerHTML = '<i class="fas fa-times"></i>';
+    newItem.classList.add("cartItem");
+    newItem.setAttribute("index", cart[]);
+    newItem.innerText = `${item.name}: $${item.price.toFixed(2)}`;
+    newItem.appendChild(deleteIcon);
+    document.querySelector("#cart").appendChild(newItem);
+    console.log(cart);
+  }*/
+}
+
+// Get the modal
+var modal = document.getElementById("myModal");
+var modal2 = document.getElementById("myModal2");
+
+// Get the button that opens the modal
+var CashBtn = document.getElementById("cash");
+var CardBtn = document.getElementById("card");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close")[1];
+
+// When the user clicks on the button, open the modal
+CashBtn.onclick = function() {
+  modal.style.display = "block";
+};
+CardBtn.onclick = function() {
+  modal2.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+};
+
+span2.onclick = function() {
+  modal2.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  } else if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+};
+
+// Delete Function
+document.querySelector("#cart").addEventListener("click", removeFromCart);
+
+function removeFromCart(e) {
+  if(e.target.classList.contains("fas")) {
+    const index = e.target.getAttribute("index");
+    cart.splice(index, 1);
   }
 }
+
